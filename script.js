0cd32097bd4541f77b1c4924f69072eca80a3437
marvolo-riddle <iovcha@icloud.com>
2024-02-23 23:08:46 +0200

    update

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let mood = "I feel happy and and good!";
-console.log(mood.replaceAll("and", "&"));
+//1 частина
+const age = prompt("Скільки тобі років?", 24);
+const birthDay = prompt("Яке число вашого народження?", 31);
+const favNumber = prompt("Ваша улюблена цифра?", 3);
+let sum = age + birthDay + favNumber;
+console.log(sum);
+//
+//
+//
+//2 частина. Я так зрозуміла, що потрібно перетворити у масив
+let numbers = "2 4 3 1 3";
+let splitNumbers = numbers.split(" ");
+console.log(splitNumbers);
