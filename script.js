0d04b1ff616cebb84b24884084f1608a5b735747
marvolo-riddle <iovcha@icloud.com>
2024-02-26 18:32:06 +0200

    перездача2

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ console.log(sum);
 //
 //
 //2 частина
-let number = 24313;
-let array = ("" + number).split("").map(Number);
-console.log(array);
+let string = "24313";
+let result1 = string.charAt(0);
+let result2 = string.charAt(1);
+let result3 = string.charAt(2);
+let result4 = string.charAt(3);
+let result5 = string.charAt(4);
+console.log(
+  result1 + " " + result2 + " " + result3 + " " + result4 + " " + result5
+);
