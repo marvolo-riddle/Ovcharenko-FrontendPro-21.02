387335373395af0de1db4b558bfe9d8546bfab5e
marvolo-riddle <iovcha@icloud.com>
2024-02-28 16:59:04 +0200

    дз3

---
+++ js/sript.js
@@ -0,0 +1,9 @@
+let number1 = +prompt("Обери перше число", 8);
+let number2 = +prompt("Обери наступне число", 2);
+//
+let sum = number1 + number2;
+let diff = number1 - number2;
+let multip = number1 * number2;
+let divis = number1 / number2;
+//
+const result = alert(sum + "," + diff + "," + multip + "," + divis);
