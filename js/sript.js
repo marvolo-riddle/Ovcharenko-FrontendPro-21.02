b863ee5fdfaea7cf46b969ed9b2468e4d02c355c
marvolo-riddle <iovcha@icloud.com>
2024-02-28 18:46:07 +0200

    ДЗ4

--- js/sript.js
+++ js/sript.js
@@ -1,9 +1,4 @@
-let number1 = +prompt("Обери перше число", 8);
-let number2 = +prompt("Обери наступне число", 2);
-//
-let sum = number1 + number2;
-let diff = number1 - number2;
-let multip = number1 * number2;
-let divis = number1 / number2;
-//
-const result = alert(sum + "," + diff + "," + multip + "," + divis);
+let secInHover = +(60 ** 2);
+let question = +prompt("Скільки годин ви працюєте?", 5);
+let sum = secInHover * question;
+let result = alert("Ви працюєте " + sum + " секунд!");
