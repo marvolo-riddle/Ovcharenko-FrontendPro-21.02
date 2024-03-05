6b102352e21f34c1349c5d857587199bea2c3cdb
marvolo-riddle <iovcha@icloud.com>
2024-03-05 20:59:42 +0200

    ДЗ5

--- js/sript.js
+++ js/sript.js
@@ -1,4 +1,6 @@
-let secInHover = +(60 ** 2);
-let question = +prompt("Скільки годин ви працюєте?", 5);
-let sum = secInHover * question;
-let result = alert("Ви працюєте " + sum + " секунд!");
+let number1 = +prompt("Select the first number", 3);
+let number2 = +prompt("Select the second number", 9);
+let number3 = +prompt("Select the third number", 6);
+let sum = number1 + number2 + number3;
+let average = sum / 3;
+let message = alert("Середнє значення ваших чисел буде " + average + "!");
