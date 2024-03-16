4aad72d9fd596edceb9c891b6c808931c3a3b2b9
marvolo-riddle <iovcha@icloud.com>
2024-03-16 18:22:53 +0200

    ДЗ6

--- js/sript.js
+++ js/sript.js
@@ -1,6 +1,49 @@
-let number1 = +prompt("Select the first number", 3);
-let number2 = +prompt("Select the second number", 9);
-let number3 = +prompt("Select the third number", 6);
-let sum = number1 + number2 + number3;
-let average = sum / 3;
-let message = alert("Середнє значення ваших чисел буде " + average + "!");
+let userBirth = prompt("Якого ти року народження?", 2000);
+if (userBirth === null) {
+  alert("Шкода, що Ви не захотіли ввести свій рік народження.");
+} else {
+  let city = prompt("У якому місті ти живеш?", "Київ");
+  if (city === null) {
+    alert("Шкода, що Ви не захотіли ввести своє місто.");
+  } else {
+    let favoriteSport = prompt("Який твій улюблений вид спорту?", "Теніс");
+    if (favoriteSport === null) {
+      alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту.");
+    } else {
+      const age = 2024 - userBirth;
+      let message = "Ваш вік: " + age + " років.\n";
+      if (city === "Київ" || city === "Вашингтон" || city === "Лондон") {
+        let country = "";
+        switch (city) {
+          case "Київ":
+            country = "України";
+            break;
+          case "Вашингтон":
+            country = "США";
+            break;
+          case "Лондон":
+            country = "Великої Британії";
+            break;
+          default:
+            country = "цієї країни";
+            break;
+        }
+        message += "Ти живеш у столиці " + country + ".";
+      } else {
+        message += "Ти живеш у місті " + city + ".";
+      }
+      let champions = {
+        футбол: "Ліонель Мессі",
+        теніс: "Рафаель Надаль",
+        формула1: "Макс Ферстаппен",
+      };
+      if (favoriteSport.toLowerCase() in champions) {
+        let championName = champions[favoriteSport.toLowerCase()];
+        message += "\nКруто! Хочеш стати " + championName + "?";
+      } else {
+        message += "\nКруто! Але хто твій улюблений спортсмен?";
+      }
+      alert(message);
+    }
+  }
+}
