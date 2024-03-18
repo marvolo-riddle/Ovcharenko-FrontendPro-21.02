70a9265b7eee3b6802af2640e78fcc2cb914ae87
marvolo-riddle <iovcha@icloud.com>
2024-03-18 15:25:25 +0200

    02

--- js/sript.js
+++ js/sript.js
@@ -1,4 +1,9 @@
-let i;
-for (i = 0; i < 5; i++) {
-  console.log(i);
+let length = parseInt(prompt("Яка буде довжина нашого масиву?"));
+let array = [];
+for (let i = 0; i < length; i++) {
+  let element = parseInt(prompt("Введіть елемент масиву:"));
+  array.push(element);
 }
+array.sort();
+array.splice(1, 3);
+alert("Масив:" + array);
