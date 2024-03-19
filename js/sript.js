0cde15b61325bbe7b9a8cc8dafccf72109fece42
marvolo-riddle <iovcha@icloud.com>
2024-03-19 14:02:26 +0200

    ДЗ10

--- js/sript.js
+++ js/sript.js
@@ -1,90 +1,55 @@
-// 1. Вивести на сторінку в один рядок через кому числа від 10 до 20.
-for (i = 10; i <= 20; i++) {
-  document.write(i);
-  if (i < 20) {
-    document.write(", ");
+// 1. Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
+for (let i = 20; i <= 30; i += 0.5) {
+  document.write(i + " ");
+}
+document.write("</br>");
+// 2. Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
+let doll = 27;
+for (i = 10; i <= 100; i += 10) {
+  document.write(doll * i + " ");
+}
+document.write("</br>");
+// 3.Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
+let number = 144;
+for (i = 1; i <= 100; i++) {
+  if (i * i <= number) {
+    document.write(i + " ");
   }
 }
-document.write("<br>");
-// 2. Вивести квадрати чисел від 10 до 20.
-for (i = 10; i <= 20; i++) {
-  document.write(i * i + ", ");
-}
-document.write("<br>");
-// 3. Вивести таблицю множення на 7.
-for (i = 1; i <= 10; i++) {
-  document.write(i * 7 + ", ");
-}
-// 4. Знайти суму всіх цілих чисел від 1 до 15.
-let sum = 0;
-for (let i = 1; i <= 15; i++) {
-  sum += i;
-}
-document.write("<br>" + "Сума усіх: " + sum);
-// 5. Знайти добуток усіх цілих чисел від 15 до 35.
-let prod = 1;
-for (let i = 15; i <= 35; i++) {
-  prod *= i;
-}
-document.write("<br>" + "Добуток: " + prod);
-// 6. Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
-let sum2 = 0;
-for (i = 1; i <= 500; i++) {
-  sum2 += i;
-}
-let average = sum2 / 500;
-document.write("<br>" + "Cереднє арифметичне: " + average);
-// 7. Вивести суму лише парних чисел в діапазоні від 30 до 80.
-let sum3 = 0;
-for (i = 30; i <= 80; i += 2) {
-  sum3 += i;
-}
-document.write("<br>" + "Сума парних: " + sum3);
-// 8. Вивести всі числа в діапазоні від 100 до 200 кратні 3.
-document.write("<br>");
-for (i = 100; i <= 200; i++) {
-  if (i % 3 === 0) {
-    document.write(i + ", ");
+document.write("</br>");
+// 4. Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
+const number3 = 30;
+let isPrime = true;
+if (number3 <= 1) {
+  isPrime = false;
+} else {
+  for (let i = 2; i < number; i++) {
+    if (number3 % i === 0) {
+      isPrime = false;
+      break;
+    }
   }
 }
-// 9. Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
-document.write("<br>");
-let number = 24;
-let divisors = [];
-document.write("Дільники числа " + number + ": ");
-for (let i = 1; i <= number; i++) {
-  if (number % i === 0) {
-    divisors.push(i);
-  }
+if (isPrime) {
+  document.write(number3 + " є простим числом.");
+} else {
+  document.write(number3 + " не є простим числом.");
 }
-document.write(divisors);
-// 10. Визначити кількість його парних дільників.
-document.write("<br>");
-let evenDivisorsCount = 0;
-document.write("Парні дільники числа " + number + ": ");
-for (let i = 1; i <= number; i++) {
-  if (number % i === 0 && i % 2 === 0) {
-    evenDivisorsCount++;
-    document.write(i + ", ");
-  }
+document.write("</br>");
+// 5. Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).
+const number4 = 243;
+let power = 0;
+let result = 1;
+while (result < number4) {
+  power++;
+  result *= 3;
 }
-document.write("<br>Кількість парних дільників: " + evenDivisorsCount);
-// 11. Знайти суму його парних дільників.
-document.write("<br>");
-let sumOfEvenDivisors = 0;
-for (let i = 1; i <= number; i++) {
-  if (number % i === 0 && i % 2 === 0) {
-    sumOfEvenDivisors += i;
-  }
-}
-document.write(
-  "Сума парних дільників числа " + number + ": " + sumOfEvenDivisors
-);
-// 12. Надрукувати повну таблицю множення від 1 до 10.
-document.write("<br>");
-for (let i = 1; i <= 10; i++) {
-  for (let j = 1; j <= 10; j++) {
-    document.write(i + " * " + j + " = " + i * j + "<br>");
-  }
-  document.write("<br>");
+if (result === number4) {
+  document.write(
+    number4 + " можна отримати шляхом зведення числа 3 у ступінь " + power + "."
+  );
+} else {
+  document.write(
+    number4 + " не можна отримати шляхом зведення числа 3 у ступінь."
+  );
 }
